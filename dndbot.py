9341a050bc6e687a279a6d6dc4452989e4820b56
Malcolm Diller <malcolm@dillerm.io>
2020-10-14 23:07:30 -0700

    updated to work with basic reckless casting

--- dndbot.py
+++ dndbot.py
@@ -1,15 +1,35 @@
 from discord.ext import commands
 import random
 import json
+import asyncio
 import os
 
 # https://discordapp.com/oauth2/authorize?permissions=2048&scope=bot&client_id=766162122404134942
+with open("settings.json", "r") as f:
+	settings = json.loads(f.read())
+
 with open("reckless_casting.json", "r") as f:
 	reckless_casting_spells = json.loads(f.read())
 
 bot = commands.Bot(command_prefix='!')
 
+timeout_seconds = 120
+reaction_waiters = {}
+
 print("started!")
+
+def get_spell(spells):
+	roll = random.randint(1, 8)
+	if roll == 7:
+		spell = "OVADRIVE"
+	else:
+		spell = spells[roll - 1]
+	return f"{roll} {spell}"
+
+@bot.event
+async def on_reaction_add(reaction, user):
+	if reaction.message.id in reaction_waiters:
+		reaction_waiters[reaction.message.id].set()
 
 @bot.command()
 async def reckless(ctx, slot : int = 0):
@@ -21,7 +41,21 @@ async def reckless(ctx, slot : int = 0):
 		return
 	spell_list = reckless_casting_spells[slot]
 
-	await ctx.send(reckless_casting_spells[3][2])
+	result =  get_spell(spell_list)
+	result += "\n" + get_spell(spell_list)
+
+	message = await ctx.send(result)
+
+	watier = asyncio.Event()
+	reaction_waiters[message.id] = watier
+
+	await asyncio.wait_for(watier.wait(), timeout_seconds)
+
+	
+
+	await ctx.send("it happened")
+
+
 
 if __name__ == '__main__':
-	bot.run('')
+	bot.run(settings.get("token"))
